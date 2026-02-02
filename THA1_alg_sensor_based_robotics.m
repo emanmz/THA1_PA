3444bf109f3e0de839fc946f767ffc05b9b21c1f
emanmz <emanzaheer@utexas.edu>
2026-02-01 19:26:01 -0600

    singularity conditions in axis angle oooooooops

--- THA1_alg_sensor_based_robotics.m
+++ THA1_alg_sensor_based_robotics.m
@@ -3,29 +3,48 @@ clear all; close all; clc;
 
 %% rotational matrix SO3 -> equivalent axis angle representation
 function [angle, axis] = rot2axis(R)
-% calculate rotation anglle
-% trace(R) = R(1,1) + R(2,2) + R(3,3)
+% check if so3
 if isSO3(R) == 0
     disp("Not a valid rotation matrix");
     return
 end
 
+% calc angle 
 cosTheta = (trace(R) - 1) / 2;
 angle = acos(cosTheta);
-n = (1/2 * sin(angle));
-n2 = [R(3,2)-R(2,3), R(1,3) - R(3,1),R(2,1) - R(1,2)];
-axis = n * n2;
+% singularity conditions and then axis calc
+if angle == 0
+    axis = NaN;
+elseif angle == pi
+    n = 1 / (sqrt(2*(1+R(3,3))));
+    n2 = [R(1,3),R(2,3),1+R(3,3)];
+    axis = n2 / n;
+else 
+    n = (1/2 * sin(angle));
+    n2 = [R(3,2)-R(2,3), R(1,3) - R(3,1),R(2,1) - R(1,2)];
+    axis = n2 / n;
+end 
 
 end
 
 %% rotational matrix -> Quaternion
 
 function [q0, q1, q2, q3] = rot2quat(R)
+% check if so3
+if isSO3(R) == 0
+    disp("Not a valid rotation matrix");
+    return
+end
+% calculate quat
 q0 = 1/2 * sqrt(R(1,1)+R(2,2)+R(3,3)+1);
 q1 = 1/2 * sign((R(3,2)-R(2,3))*sqrt(R(1,1)-R(2,2)-R(3,3)+1));
 q2 = 1/2 * sign((R(1,3)-R(3,1))*sqrt(R(2,2)-R(3,3)-R(1,1)+1));
 q3 = 1/2 * sign((R(2,1)-R(1,2))*sqrt(R(3,3)-R(1,1)-R(2,2)+1));
 end 
+
+%% rotation matrix -> ZYZ
+
+
 
 %% Rotation matrix check
 
@@ -47,21 +66,27 @@ end
 
 R = [0 -1 0; 1 0 0; 0 0 1]; % 90 degree rotation around Z
 [angle, axis] = rot2axis(R);
-[q0, q1, q2, q3] = rot2quat(R)
+[q0, q1, q2, q3] = rot2quat(R);
 
-% Displaying Everything 
+% Displaying rotation 
 disp("Rotational Matrix");
 disp(R)
+
+% Dispaly axis angle 
 disp("Axis");
 disp(axis);
 disp("Angle");
 disp(angle);
+
+% Display Quat
 disp("Quat");
 disp(q0);
 disp(q1);
 disp(q2);
 disp(q3);
 
+
 if isSO3(R)
     disp("so3 check is true");
 end 
+
